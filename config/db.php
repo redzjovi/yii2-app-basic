3cdc793da0a8955e454c0d55f5f3254011f56713
Jovi <jovi.anto@misteraladin.com>
2022-03-12 14:24:34 +0700

    [Config] set db.

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=mysql;dbname=yii2-app-basic',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
