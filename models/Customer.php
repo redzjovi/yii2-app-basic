ffb7e2600bdcae2773e6bfc6f71a7130a33d8901
Jovi <jovi.anto@misteraladin.com>
2022-03-12 14:24:54 +0700

    [Customer] add crud.

---
+++ models/Customer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "customers".
+ *
+ * @property int $id
+ * @property string $name
+ */
+class Customer extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'customers';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+}
